76b506dc370d5b23971b6138339a5da56969f0c9
Jessica Donzelli <jedonzelli@gmail.com>
2020-09-10 15:37:33 +0200

    inserito codice js x ricerca contatti

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-// FUNZIONE PER INSERIRE I MIEI MESSAGGI
 $(document).ready(function() {
 
+// FUNZIONE PER INSERIRE I MIEI MESSAGGI
 $(".send-message").click(
   function() {
     sendMessage();
   }
 );
-
 $("#input-message").keyup(
   function(event) {
     if (event.which == 13) {
@@ -14,36 +13,61 @@ $("#input-message").keyup(
     }
   }
 );
-});
 
 function sendMessage() {
   var inputText = $("#input-message").val();
-
   if(inputText != "") {
     var templateMessage = $(".templates .message-row").clone();
-
     var date = new Date();
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var time = hours + ":" + minutes;
-
     templateMessage.find(".message-text").text(inputText);
     templateMessage.find(".message-time").text(time);
     templateMessage.addClass("sent");
-
     $(".chat").append(templateMessage);
     $("#input-message").val("");
-  }
-}
 // /FUNZIONE PER INSERIRE I MIEI MESSAGGI
 
-// FUNZIONE PER CERCARE CONTATTO
-$(".input-contact").keypress(
+
+
+// FUNZIONE PER RICEVERE RISPOSTE AUTOMATICHE
+    setTimeout(receivedMessage, 1000);
+  }
+}
+      function receivedMessage() {
+        var receivedMessage = $(".templates .message-row").clone();
+        receivedMessage.find(".message-text").text("OK");
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var time = hours + ":" + minutes;
+        receivedMessage.find(".message-time").text(time);
+        $(".chat").append(receivedMessage);
+      }
+// FUNZIONE PER RICEVERE RISPOSTE AUTOMATICHE
+
+
+
+// FUNZIONE PER CERCARE UN CONTATTO
+$(".input-form").keypress(
   function() {
     searchContact();
   }
 );
 
 function searchContact() {
-  var inputContact = $(".input-contact").val();
+  var inputContent = $(".input-form").val();
+  var contacts = $(".contacts-text h4").text();
+
+  if (contacts.includes(inputContent)) {
+    $(".contacts-box").hide();
+  }
+  else {
+    $(".contacts-box").show();
+  }
 }
+// /FUNZIONE PER CERCARE UN CONTATTO
+
+// GRAFFA FINE FUNZIONE GENERALE
+});
